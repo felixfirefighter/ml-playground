27f0b6b109990775a9da266e4b5d11f619188c0c
Yoong Ti <leeyoongti@gmail.com>
2023-01-21 21:52:47 +0800

    add get-real-1

---
+++ ml-fundamental/10-get-real-1/mnist.py
@@ -0,0 +1,33 @@
+# An MNIST loader.
+import numpy as np
+import gzip
+import struct
+
+def load_images(filename):
+    # Open and unzip the file of images:
+    with gzip.open(filename, 'rb') as f:
+        # Read the header information into a bunch of variables
+        _ignored, n_images, columns, rows = struct.unpack('>IIII', f.read(16))
+        # Read all the pixels into a NumPy array of bytes:
+        all_pixels = np.frombuffer(f.read(), dtype=np.uint8)
+        # Reshape the pixels into a matrix where each line is an image:
+        return all_pixels.reshape(n_images, columns * rows)
+
+def prepend_bias(X):
+    # Insert a column of 1s in the position 0 of X.
+    # (“axis=1” stands for: “insert a column, not a row”)
+    return np.insert(X, 0, 1, axis=1)
+
+def load_labels(filename):
+  # Open and unzip the file of images:
+  with gzip.open(filename, 'rb') as f:
+    # Skip the header bytes:
+    f.read(8)
+    # Read all the labels into a list:
+    all_labels = f.read()
+    # Reshape the list of labels into a one-column matrix:
+    return np.frombuffer(all_labels, dtype=np.uint8).reshape(-1, 1)
+
+def encode_fives(Y):
+  # Convert all 5s to 1, and everything else to 0
+  return (Y == 5).astype(int)
